d15309227fef74de3b91314b0dafc04c2258cabb
ajgil <antoniojesusgil@gmail.com>
2021-12-21 12:31:46 +0100

    no token users

--- src/components/Chat/HiklubChat.js
+++ src/components/Chat/HiklubChat.js
@@ -1,69 +1,92 @@
-import React, { useEffect, useState } from "react";
-import { StreamChat } from 'stream-chat';
-import { Chat, Channel, ChannelHeader, ChannelList, LoadingIndicator, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import { useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  VirtualizedMessageList,
+  ChannelHeader,
+  ChannelList,
+  MessageInput,
+  Window,
+  Thread,
+  MessageSimple
+} from "stream-chat-react";
 
-import 'stream-chat-react/dist/css/index.css';
+import "@stream-io/stream-chat-css/dist/css/index.css";
+import "./styles.css";
 
-/*
-const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZmFuY3ktaGFsbC0zIiwiZXhwIjoxNjQwMDE2NjAxfQ.9RyIYveYaayBHUBKEk66Xn_xihZlY2BBmGzeixujwxk';
+const API_KEY = "dj4bn8c565ua";
 
-var stream = require('getstream');
-// Instantiate a new client (server side)
-client = stream.connect('7rfypad6gc33', 'qy6epb64gwckyjgt3bwwwrh2qq72gtgtdhuufgzu5apjmqkkd6nbd6t394ps2av8', '1151626');
-// Instantiate a new client (client side)
-client = stream.connect('7rfypad6gc33', null, '1151626');
-// Find your API keys here https://getstream.io/dashboard/
-*/
+// Setup two users, so that we can simulate send/receive messages using two urls:.
+// https://m3j4h.csb.app/
+// https://m3j4h.csb.app/?alt
 
-//const client = StreamChat.getInstance('dj4bn8c565ua', '6nm2z8whbex2m5qtf4vqbeyn2u3y6wtfctskmbpz5wewe4t4b2hz3uufwjsucqum')
-const client = new StreamChat('dj4bn8c565ua', '6nm2z8whbex2m5qtf4vqbeyn2u3y6wtfctskmbpz5wewe4t4b2hz3uufwjsucqum')
-const userid = "artis"
-const token = client.createToken(userid)
+const USER_ID1 = "ajgil";
 
-client.connectUser(
+//const USER_TOKEN1 =
+//"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicmlja3kifQ.GtkIUZC_XpaaafSISkVyIXE3ecArf5Dt0RPl_6Tg9qc";
+
+const USER_ID2 = "artis";
+//const USER_TOKEN2 =
+//"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoianVsaWFuIn0.iQ7y2r5OsWPENdS0EiNOXD-5PxZhW85uweFYDZQRCjw";
+
+const chatClient = StreamChat.getInstance(API_KEY);
+
+const USER_TOKEN1 = chatClient.createToken(USER_ID1);
+const USER_TOKEN2 = chatClient.createToken(USER_ID2);
+
+const alt = window.location.search === "?alt";
+const userId = alt ? USER_ID2 : USER_ID1;
+const userToken = alt ? USER_TOKEN2 : USER_TOKEN1;
+
+document.title = `${userId} - Stream test`;
+
+chatClient.connectUser(
   {
-    id: 'artis',
-    name: 'artis',
-    image: 'https://getstream.io/random_png/?id=artis&name=artis',
+    id: userId,
+    name: userId,
+    image: `https://getstream.io/random_png/?id=${userId}&name=${userId}`
   },
+  userToken
 );
 
-const channel = client.channel('messaging', 'custom_channel_id', {
-  // add as many custom fields as you'd like
-  image: 'https://www.drupal.org/files/project-images/react.png',
-  name: 'Talk about Hiklub',
-  members: ['artis'],
+const channel = chatClient.channel("messaging", "hallway", {
+  members: [USER_ID1, USER_ID2]
 });
+channel.watch();
 
-const filters = { type: 'messaging', members: { $in: ['artis'] } };
-const sort = { last_message_at: -1 };
-
-const HiklubChat = () => {
-
-  //const [chatClient, setChatClient] = useState(null);
-
-
-  /*
-  const channel = chatClient.channel('messaging', 'travel', { 
-    name: 'Awesome channel about traveling', 
-  }); 
-
-  */
-
-  return (
-  <Chat client={client} theme='messaging light'>
-     <ChannelList filters={filters} sort={sort} />
-      <Channel channel={channel}>
-        <Window>
-          <ChannelHeader />
-          <MessageList />
-          <MessageInput />
-        </Window>
-        <Thread />
-      </Channel>
+/**
+ * Set defaultItemHeight to the pixel height of your one-liner message.
+ * This speeds up rendering and minimizes reflows.
+ *
+ * If you're using a custom message, ensure that reactions don't change the height of the item.
+ * This improves the end-user experience and performance, as the chat "bounses" less.
+ *
+ * The `increaseViewportBy` decreases the rerenders for the message sender,
+ * and eliminates short "blinks" when new messages appear.
+ *
+ * stickToBottomScrollBehavior can be either 'smooth' or 'auto'
+ * smooth may have hard time catching up with many incoming messages. Use 'auto' for busy chats.
+ */
+const HiklubChat = () => (
+  <Chat client={chatClient}>
+    <ChannelList />
+    <Channel>
+      <Window>
+        <ChannelHeader />
+        <VirtualizedMessageList
+          stickToBottomScrollBehavior="auto"
+          message={MessageSimple}
+          defaultItemHeight={60}
+          additionalVirtuosoProps={{
+            increaseViewportBy: { top: 0, bottom: 200 }
+          }}
+        />
+        <MessageInput focus />
+      </Window>
+      <Thread />
+    </Channel>
   </Chat>
-  )
-
-}
+);
 
 export default HiklubChat;
