9366e867b889469011e88f2150f53264c2d157c9
ajgil <antoniojesusgil@gmail.com>
2021-12-22 09:32:35 +0100

    info updated

--- src/supabase.js
+++ src/supabase.js
@@ -4,10 +4,6 @@
 // https://supabase.io/docs/guides/examples
 //https://ruanmartinelli.com/posts/supabase-authentication-react
 
-
-// Supabase JWT Secret 
-// Used to decode your JWTs You can also use this to mint your own JWTs
-// a6bc41f9-a1a7-4278-932c-938dcfaab1af
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
