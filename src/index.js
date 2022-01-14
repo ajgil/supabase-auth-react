e2f08e065c39126502a65e92f936cb38d75c4088
ajgil <antoniojesusgil@gmail.com>
2022-01-14 10:54:20 +0100

    renaming

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
-import App from './pages/App';
-import reportWebVitals from './reportWebVitals';
-import Footer from './components/footer'
-import Navbar from './components/navbar';
+import App from './pages/App'
+import reportWebVitals from './reportWebVitals'
+import Footer from './components/Footer'
+import Navbar from './components/NavBar'
 
 ReactDOM.render(
   <BrowserRouter>
